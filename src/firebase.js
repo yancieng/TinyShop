3967e388559603eb810c523af58b00a13fada155
Yancie Ng <yancie.ng@binance.com>
2022-03-17 16:12:26 +1000

    setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsp4-rvPIRNY-YhBy50ymSb0qY8C23NNk",
+  authDomain: "tinyshop-666a8.firebaseapp.com",
+  projectId: "tinyshop-666a8",
+  storageBucket: "tinyshop-666a8.appspot.com",
+  messagingSenderId: "399858454250",
+  appId: "1:399858454250:web:192d096faa3dea6114d193",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
